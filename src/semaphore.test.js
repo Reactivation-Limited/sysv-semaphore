bdcacddfa3114eb2f89564174881a1767dbec26e
Jon Cooke <reactivationlimited@icloud.com>
2024-05-09 15:34:59 +0100

    it fails :(

--- src/semaphore.test.js
+++ src/semaphore.test.js
@@ -1,10 +1,25 @@
-const { Semaphore } = require('../build/Release/OSX.node');
+const { Semaphore, o_creat, o_excl } = require('../build/Release/OSX.node');
 
-console.log(Semaphore);
+const name = 'semaphore-test';
 
+console.log('Semaphore', Semaphore, o_creat, o_excl);
 
 describe('Semaphore', () => {
   it('should be a class', () => {
-    expect(new Semaphore).toBeInstanceOf(Semaphore);
-  })
-})
+    expect(new Semaphore()).toBeInstanceOf(Semaphore);
+  });
+
+  describe('sempahore open and close', () => {
+    let semaphore;
+    beforeEach(() => {
+      semaphore = new Semaphore();
+    });
+    afterEach(() => {
+      // expect(() => semaphore.unlink()).not.toThrow();
+    });
+
+    it.only('should open a semaphore', () => {
+      expect(() => semaphore.open(name)).not.toThrow();
+    });
+  });
+});
