e588144592dfa12411a9c75823bd4afc10a62b97
Jon Cooke <reactivationlimited@icloud.com>
2024-05-09 15:15:57 +0100

    modes for system object creation

--- src/mode.cpp
+++ src/mode.cpp
@@ -1,22 +1,21 @@
+static const int s_irwxu;
+static const int s_irusr;
+static const int s_iwusr;
+static const int s_ixusr;
 
-const int s_irwxu = S_IRWXU;
-const int s_irusr = S_IRUSR;
-const int s_iwusr = S_IWUSR;
-const int s_ixusr = S_IXUSR;
+static const int s_irwxg;
+static const int s_irgrp;
+static const int s_iwgrp;
+static const int s_ixgrp;
 
-const int s_irwxg = S_IRWXG;
-const int s_irgrp = S_IRGRP;
-const int s_iwgrp = S_IWGRP;
-const int s_ixgrp = S_IXGRP;
+static const int s_irwxo;
+static const int s_iroth;
+static const int s_iwoth;
+static const int s_ixoth;
 
-const int s_irwxo = S_IRWXO;
-const int s_iroth = S_IROTH;
-const int s_iwoth = S_IWOTH;
-const int s_ixoth = S_IXOTH;
+static const int s_isuid;
+static const int s_isgid;
+static const int s_isvtx;
 
-const int s_isuid = S_ISUID;
-const int s_isgid = S_ISGID;
-const int s_isvtx = S_ISVTX;
-
-const int o_creat = O_CREAT;
-const int o_excl = O_EXCL;
+static const int o_creat;
+static const int o_excl;
--- src/mode.h
+++ src/mode.h
@@ -1,21 +1,21 @@
-static const int s_irwxu;
-static const int s_irusr;
-static const int s_iwusr;
-static const int s_ixusr;
+static const int s_irwxu = S_IRWXU;
+static const int s_irusr = S_IRUSR;
+static const int s_iwusr = S_IWUSR;
+static const int s_ixusr = S_IXUSR;
 
-static const int s_irwxg;
-static const int s_irgrp;
-static const int s_iwgrp;
-static const int s_ixgrp;
+static const int s_irwxg = S_IRWXG;
+static const int s_irgrp = S_IRGRP;
+static const int s_iwgrp = S_IWGRP;
+static const int s_ixgrp = S_IXGRP;
 
-static const int s_irwxo;
-static const int s_iroth;
-static const int s_iwoth;
-static const int s_ixoth;
+static const int s_irwxo = S_IRWXO;
+static const int s_iroth = S_IROTH;
+static const int s_iwoth = S_IWOTH;
+static const int s_ixoth = S_IXOTH;
 
-static const int s_isuid;
-static const int s_isgid;
-static const int s_isvtx;
+static const int s_isuid = S_ISUID;
+static const int s_isgid = S_ISGID;
+static const int s_isvtx = S_ISVTX;
 
-static const int o_creat;
-static const int o_excl;
+static const int o_creat = O_CREAT;
+static const int o_excl = O_EXCL;
