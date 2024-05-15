7d9909b90f8d6f7e2988dc6173cf80888932a866
Jon Cooke <reactivationlimited@icloud.com>
2024-05-15 22:13:47 +0100

    add child to the debug script

--- debug/sem-sysv.js
+++ debug/sem-sysv.js
@@ -1,5 +1,8 @@
 const { open, unlink } = require('node:fs/promises');
+const { fork } = require('node:child_process');
+
 const { SemaphoreV: Semaphore } = require('../build/Debug/OSX.node');
+const childMessages = require('../test/parent.js');
 
 console.log(Semaphore);
 
@@ -24,9 +27,33 @@ const test = async () => {
     if (!s.trywait()) {
       throw 'trywait false';
     }
+    console.log('refs', s.refs());
     s2.close();
+    console.log('refs', s.refs());
     s.close(); // should unlink it
-    s.post();
+
+    console.log('child sem stuff');
+    const s3 = Semaphore.createExclusive(name, 0o600, 1);
+    console.log('parent', s3.refs());
+    const child = fork('./test/semaphore-sysv-child.js', ['child'], {
+      stdio: [process.stdin, process.stdout, process.stderr, 'ipc'],
+      env: { DEBUG_COLORS: '', DEBUG: process.env.DEBUG }
+    });
+
+    process.on('SIGINT', () => {
+      child.kill('SIGINT');
+    });
+
+    const messages = childMessages(child);
+    const { value } = await messages.next();
+    if (value !== name) {
+      console.log('parent', name, 'child', value);
+    }
+    console.log('parent', s3.refs());
+    child.kill('SIGTERM');
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log('parent', s3.refs());
+    s3.close();
   } catch (e) {
     console.log('error', e);
   } finally {
@@ -34,7 +61,11 @@ const test = async () => {
     try {
       Semaphore.unlink(name);
     } catch (error) {
-      console.log('close did not unlink');
+      if (error.message === 'ENOENT') {
+        console.log('already unlinked');
+      } else {
+        console.log(error);
+      }
     }
     await F.close();
     await unlink(name);
