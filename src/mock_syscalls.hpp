d6e86cdc0fc279f6517dd3d88be18f9870b927c7
Jon Cooke <reactivationlimited@icloud.com>
2025-04-17 16:20:37 +0100

    linux needs semun to be declared, make sembuf const

--- src/mock_syscalls.hpp
+++ src/mock_syscalls.hpp
@@ -6,6 +6,15 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
+#ifdef _SEM_SEMUN_UNDEFINED
+union semun {
+  int val;               /* Value for SETVAL */
+  struct semid_ds *buf;  /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short *array; /* Array for GETALL, SETALL */
+  struct seminfo *__buf; /* Buffer for IPC_INFO (Linux-specific) */
+};
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,7 +39,7 @@ typedef struct {
 
     struct {
       int semid;
-      struct sembuf *sops;
+      const struct sembuf *sops;
       size_t nsops;
     } semop_args;
 
